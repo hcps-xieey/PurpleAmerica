ad02eea5032e1292e9996c476140a17503d58b94
cmneville <cmneville@180CMNEVILLE.hcps.internal>
2016-04-15 12:05:09 -0400

    Added Test Class and made slight adjustments to the framework.

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -1,7 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Political Map Project
+ * Name: 
+ * Block:
+ * 
+ * Program Purpose:
+ *
+ * Algorithm:
+ * 
+ * Future/possible improvements:
+ *
  */
 package map;
 import edu.princeton.cs.introcs.*;
