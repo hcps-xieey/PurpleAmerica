4eaa998b3d22d9690e13b8f7501f830c53fe1824
cmneville <cmneville@180CMNEVILLE.hcps.internal>
2016-04-15 12:01:21 -0400

    Added Test Class and made slight adjustments to the framework.

---
+++ src/map/PoliticalMap.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package map;
+import edu.princeton.cs.introcs.*;
+/**
+ *
+ * @author cmneville
+ */
+public class PoliticalMap {
+    public static void main(String[] args){
+        StdDraw.setCanvasSize(1000, 1000);// set the size of the window.
+        
+    }
+}
