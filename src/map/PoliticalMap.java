7bc23849eeb6ece261ed5351c011e7f8dd472bd7
hcps-sreekeshs <hcps-sreekeshs@180-17SREEKESHS.hcps.internal>
2016-04-25 14:45:10 -0400

    Draws, doesn't read in file

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -12,13 +12,27 @@
  */
 package map;
 import edu.princeton.cs.introcs.*;
+import java.util.Scanner; 
+import java.io.*; 
 /**
  *
  * @author 
  */
 public class PoliticalMap {
-    public static void main(String[] args){
-        
-        
+    public static void main(String[] args) throws Exception{
+//***********Variable Declarations **************************//
+        double coordinates;
+//***************Constructor******************************// 
+        //File USA = new File("USA.txt"); 
+        //Scanner scan = new Scanner(USA);
+        //coordinates = scan.nextDouble();
+//*********************Public Methods**********************// 
+        StdDraw.setCanvasSize(1000,1000);//sets the size of the window 
+        StdDraw.setPenRadius(0.005);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.point(0.5, 0.5);
+//*******************Private Methods***********************//
+         
+          
     }
 }
