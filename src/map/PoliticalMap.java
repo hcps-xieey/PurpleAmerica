a218499c14bbbead7f605e04261c930deaff82b7
hcps-xieey <hcps-xieey@180-18XIEEY.hcps.internal>
2016-04-25 14:44:48 -0400

    Hello World

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -11,14 +11,44 @@
  *
  */
 package map;
-import edu.princeton.cs.introcs.*;
+import static edu.princeton.cs.introcs.StdDraw.setCanvasSize;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
 /**
  *
  * @author 
  */
 public class PoliticalMap {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException, URISyntaxException{
+        File test = new File(PoliticalMap.class.getResource("/map/USA.txt").toURI()); 
+        Scanner sc = new Scanner(test); 
+        sc.nextInt(); 
+        sc.nextInt(); 
+        sc.nextInt(); 
+        sc.nextInt(); 
+        sc.nextInt(); 
         
+        sc.nextLine();
+        sc.nextLine();
+        sc.nextInt(); 
+        
+        double[] ary = new double[10000];
+        double[] ary2 = new double[10000]; 
+        double input; 
+        int counter = 0; 
+        while(sc.hasNextDouble()){
+            input = sc.nextInt(); 
+            ary[counter] = input; 
+            input = sc.nextInt(); 
+            ary2[counter] = input; 
+            counter ++; 
+        }
+        
+        setCanvasSize(1000,1000); 
         
     }
 }
