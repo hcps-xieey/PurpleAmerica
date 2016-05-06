d31bb1b113a68a1ba2ff2b5deef7ca7668a28992
hcps-sreekeshs <hcps-sreekeshs@180-17SREEKESHS.hcps.internal>
2016-05-06 14:42:57 -0400

    Draws, doesn't read in file

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -14,25 +14,69 @@ package map;
 import edu.princeton.cs.introcs.*;
 import java.util.Scanner; 
 import java.io.*; 
+import java.io.File;
+import java.util.*;
+
 /**
  *
  * @author 
  */
 public class PoliticalMap {
     public static void main(String[] args) throws Exception{
-//***********Variable Declarations **************************//
-        double coordinates;
-        File USA = new File("USA.txt"); 
-        Scanner scan = new Scanner(System.in); 
-        System.out.print(USA);
-//*********************Public Methods**********************// 
-        StdDraw.setCanvasSize(1000,1000);//sets the size of the window 
-        StdDraw.setPenRadius(0.005);
-        StdDraw.setPenColor(StdDraw.BLUE);
-        StdDraw.point(0.5, 0.5); 
-      //  StdDraw.polygon(x, y);
-//*******************Private Methods***********************//
          
           
+        
+     File file = new File("src/map/USA.txt");
+     Scanner scan = new Scanner(file);
+     double minX = scan.nextDouble(); 
+     double minY = scan.nextDouble(); 
+     double mostX = scan.nextDouble(); 
+     double mostY = scan.nextDouble(); 
+     StdDraw.setCanvasSize(1100, 700);
+     StdDraw.setXscale(minX, mostX);
+     StdDraw.setYscale(minY, mostY);
+     double[] x;
+     double[] y;     
+     int terNum;
+     int i = 0;
+     StdDraw.setPenColor(StdDraw.BLUE);
+ 
+     
+     while(scan.hasNext()){
+         try{
+             terNum = scan.nextInt();
+             x = new double[terNum];
+             y = new double[terNum];
+             for(i = 0; i < terNum; i++){
+                scan.nextLine();
+                x[i] = scan.nextDouble();
+                y[i] = scan.nextDouble();
+             }
+             StdDraw.filledPolygon(x, y);
+             
+         }catch(InputMismatchException e){
+            scan.nextLine();
+         }
+         
     }
+     scan = new Scanner(file);
+     StdDraw.setPenColor(StdDraw.BLACK);
+          while(scan.hasNext()){
+         try{
+             terNum = scan.nextInt();
+             x = new double[terNum];
+             y = new double[terNum];
+             for(i = 0; i < terNum; i++){
+                scan.nextLine();
+                x[i] = scan.nextDouble();
+                y[i] = scan.nextDouble();
+             }
+             StdDraw.polygon(x, y);
+             
+         }catch(InputMismatchException e){
+            scan.nextLine();
+         }
+         
+    }
+  }
 }
