7a28940492bd863b64af952b7f6bb21d2eef3efb
BashMash <hcps-mosavellb@henricostudents.org>
2016-05-11 15:07:44 -0400

    This main class will work with new file

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -21,58 +21,15 @@ import java.util.*;
 public class PoliticalMap {
     public static void main(String[] args) throws Exception{
         
-     File file = new File("src/map/USA.txt");
-     Scanner scan = new Scanner(file);
-     double minX = scan.nextDouble(); 
-     double minY = scan.nextDouble(); 
-     double mostX = scan.nextDouble(); 
-     double mostY = scan.nextDouble(); 
-     StdDraw.setCanvasSize(1100, 700);
-     StdDraw.setXscale(minX, mostX);
-     StdDraw.setYscale(minY, mostY);
-     double[] x;
-     double[] y;     
-     int terNum;
-     int i = 0;
-     StdDraw.setPenColor(StdDraw.BLUE);
- 
-     
-     while(scan.hasNext()){
-         try{
-             terNum = scan.nextInt();
-             x = new double[terNum];
-             y = new double[terNum];
-             for(i = 0; i < terNum; i++){
-                scan.nextLine();
-                x[i] = scan.nextDouble();
-                y[i] = scan.nextDouble();
-             }
-             StdDraw.filledPolygon(x, y);
-             
-         }catch(InputMismatchException e){
-            scan.nextLine();
-         }
-         
-    }
-     scan = new Scanner(file);
-     StdDraw.setPenColor(StdDraw.BLACK);
-          while(scan.hasNext()){
-         try{
-             terNum = scan.nextInt();
-             x = new double[terNum];
-             y = new double[terNum];
-             for(i = 0; i < terNum; i++){
-                scan.nextLine();
-                x[i] = scan.nextDouble();
-                y[i] = scan.nextDouble();
-             }
-             StdDraw.polygon(x, y);
-             
-         }catch(InputMismatchException e){
-            scan.nextLine();
-         }
-         
-    }
+    File file = new File("src/map/USA.txt");
+    File elect = new File("src/data/USA2012.txt");
+   
+    mapMake map = new mapMake(file, elect);    
+    map.mapColor();
+    map.mapBorder();
+    
+
+
      
     }
 }
