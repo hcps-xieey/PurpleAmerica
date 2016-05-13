95ce3019521f3ce8ac844f41b0779a807c5a2a2f
hcps-xieey <hcps-xieey@180-18XIEEY.hcps.internal>
2016-05-13 14:30:06 -0400

    Getting classes to be the same so it merges

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -11,87 +11,26 @@
  *
  */
 package map;
-import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.*;
 import java.io.File;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 /**
  *
  * @author 
  */
 public class PoliticalMap {
     public static void main(String[] args) throws Exception{
-          
-        File file = new File("src/map/USA.txt");
-        File file2 = new File("src/data/USA2012.txt"); 
-        Scanner scan = new Scanner(file);
-        double minX = scan.nextDouble(); 
-        double minY = scan.nextDouble(); 
-        double mostX = scan.nextDouble(); 
-        double mostY = scan.nextDouble(); 
-        StdDraw.setCanvasSize(1100, 700);
-        StdDraw.setXscale(minX, mostX);
-        StdDraw.setYscale(minY, mostY);
-        double[] x;
-        double[] y;     
-        int terNum;
-        int i = 0;
-        StdDraw.setPenColor(StdDraw.BLUE);
-
         
-        while(scan.hasNext()){
-            try{
-                terNum = scan.nextInt();
-                x = new double[terNum];
-                y = new double[terNum];
-                for(i = 0; i < terNum; i++){
-                  scan.nextLine();
-                    x[i] = scan.nextDouble();
-                    y[i] = scan.nextDouble();
-                }
-                StdDraw.filledPolygon(x, y);
+    File file = new File("src/map/USA.txt");
+    File elect = new File("src/data/USA2012.txt");
+   
+    mapMake map = new mapMake(file, elect);  
+    map.getVotes(elect);
+    map.mapColor();
+    map.mapBorder();
+    //HELLO WORLD
 
-            }catch(InputMismatchException e){
-               scan.nextLine();
-            }
 
-        }
-        scan = new Scanner(file);
-        StdDraw.setPenColor(StdDraw.BLACK);
-        while(scan.hasNext()){
-            try{
-                terNum = scan.nextInt();
-                x = new double[terNum];
-                y = new double[terNum];
-                for(i = 0; i < terNum; i++){
-                    scan.nextLine();
-                    x[i] = scan.nextDouble();
-                    y[i] = scan.nextDouble();
-                }
-                  StdDraw.polygon(x, y);
-
-            }catch(InputMismatchException e){
-                scan.nextLine();
-            }
-
-        }
-        
-        Scanner sc = new Scanner(file2); 
-        sc.nextLine(); 
-        String testInput = sc.next();
-        String[] ary = testInput.split(",");
-        double a1 = (double)(Integer.parseInt(ary[1])); 
-        double a2 = (double)(Integer.parseInt(ary[2]));
-        double a3 = (double)(Integer.parseInt(ary[3])); 
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
-        double r = ((a1)/(a1 + a2 + a3));
-        double g = ((a2)/(a1 + a2 + a3));
-        double b = ((a3)/(a1 + a2 + a3)); 
-        int red = (int)(r*255); 
-        int green = (int)(g*255);
-        int blue = (int)(b*255); 
-        System.out.println(red + ", " + green + ", " + blue);
+     
     }
 }
