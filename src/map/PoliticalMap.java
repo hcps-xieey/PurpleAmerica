9fdcc323e55e9d5b94348e9128b6cdcc4d82ca54
hcps-xieey <hcps-xieey@180-18XIEEY.hcps.internal>
2016-05-24 10:47:02 -0400

    Added code to handle input for year

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -28,6 +28,38 @@ public class PoliticalMap {
         "MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI",
         "SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"}; 
         //array with all the names of the States' abbreviations
+    String finalYear = ""; 
+    Scanner sc = new Scanner(System.in); 
+    boolean isInputvalid = false; 
+    while(!isInputvalid){
+        System.out.println("Enter the year that you would like to see: ");
+        int year = sc.nextInt(); 
+        try {
+
+            if(year >= 1960 && year <= 2012){
+                isInputvalid = true; 
+                if(year % 4 == 0){
+                        isInputvalid = true; 
+                         finalYear = year + ".txt";  
+                    } else {
+                        System.out.println("That is not a valid input");
+                        System.out.println("Your options are: 1960, 1964, 1968, 1972, 1976, 1980, 1992, 1996, 2000, 2004, 2008, 2012"); 
+                        isInputvalid = false; 
+                    }
+            } else { 
+                System.out.println("That is not a valid input"); 
+                System.out.println("Your options are: 1960, 1964, 1968, 1972, 1976, 1980, 1992, 1996, 2000, 2004, 2008, 2012");
+                isInputvalid = false; 
+            }
+
+        } catch (InputMismatchException e){
+            System.out.println("That is not a valid input"); 
+            System.out.println("Your options are: 1960, 1964, 1968, 1972, 1976, 1980, 1992, 1996, 2000, 2004, 2008, 2012");
+            
+        }
+        
+    } 
+    
     
     StdDraw.setCanvasSize(1100, 700);
      
@@ -39,7 +71,7 @@ public class PoliticalMap {
     // Calls strState and calls methods involed from CountiesMap class.   
     for (int i=0;i<strState.length;i++){
         File countyFile = new File ("src/data/" + strState[i]+ ".txt");
-        File countyVotes = new File ("src/data/"+ strState[i]+"2012.txt");
+        File countyVotes = new File ("src/data/"+ strState[i]+ finalYear);
         CountiesMap county = new CountiesMap(countyFile,countyVotes); 
         county.getVotes(countyVotes);
         county.mapColor(); 
